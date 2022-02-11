5980889afedc8cbe3c97985a740e1779b0c229b1
Your Name <you@example.com>
2022-02-10 21:47:27 -0600

    assignment3

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Tables from './Tables.jsx';
+
+
+const App =()=>{
+    return <Tables />
+  };
+
+ReactDOM.render(
+    <div><center>
+        <App />
+
+    </center>
+        
+    </div>
+    ,
+    document.getElementById("root") 
+);
